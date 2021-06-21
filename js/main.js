b425a2e52572b840d14f41f035482a766b771d89
shantam8 <dscharmen@aol.com>
2021-06-21 23:36:16 +0200

    mobile done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
+let nav = document.querySelector("#mobileNav");
+
+
+function toggleMenu(){
+    console.log("nav");
+
+    if(nav.style.opacity == 1){
+        nav.style.opacity = 0
+    }else{
+
+        nav.style.opacity = 1;
+    }
+
+}
+
+
 function init(){
     console.log("hi");
+    document.querySelector("#toggleMobileMenu").addEventListener("click", toggleMenu);
 }
 
 

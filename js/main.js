c8b0a5eaa151dbf74b3e09b5c3b2d77570c1c09a
shantam8 <dscharmen@aol.com>
2021-06-22 18:03:11 +0200

    restart main

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
 let nav = document.querySelector("#mobileNav");
+let body = document.querySelector("body");
 
-
-function toggleMenu(){
-    console.log("nav");
-
+function toggleMenu() {
+  console.log("nav");
+  /* 
     if(nav.style.opacity == 1){
         nav.style.opacity = 0
     }else{
 
         nav.style.opacity = 1;
-    }
+    } */
 
+  if (nav.classList.contains("opacityOne")) {
+    nav.classList.remove("opacityOne");
+    body.classList.remove("overflowHidden");
+  } else {
+      nav.classList.add("opacityOne");
+      body.classList.add("overflowHidden");
+  }
 }
 
-
-function init(){
-    console.log("hi");
-    document.querySelector("#toggleMobileMenu").addEventListener("click", toggleMenu);
+function init() {
+  console.log("hi");
+  document
+    .querySelector("#toggleMobileMenu")
+    .addEventListener("click", toggleMenu);
 }
-
 
 window.onload = init;

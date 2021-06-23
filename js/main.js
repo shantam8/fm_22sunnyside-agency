43744281faa47b78cc16097e68f6d9dc7d0147f8
shantam8 <dscharmen@aol.com>
2021-06-23 12:44:18 +0200

    minor code refactoring

--- js/main.js
+++ js/main.js
@@ -3,20 +3,13 @@ let body = document.querySelector("body");
 
 function toggleMenu() {
   console.log("nav");
-  /* 
-    if(nav.style.opacity == 1){
-        nav.style.opacity = 0
-    }else{
-
-        nav.style.opacity = 1;
-    } */
 
   if (nav.classList.contains("opacityOne")) {
     nav.classList.remove("opacityOne");
     body.classList.remove("overflowHidden");
   } else {
-      nav.classList.add("opacityOne");
-      body.classList.add("overflowHidden");
+    nav.classList.add("opacityOne");
+    body.classList.add("overflowHidden");
   }
 }
 
